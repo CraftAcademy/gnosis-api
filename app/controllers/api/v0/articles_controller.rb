bce746f7c3448bf08df84fe5b4048005e6595d32
erka <erik.westerberg@hotmail.se>
2019-08-28 19:53:26 +0200

    adds city as scope for article

--- app/controllers/api/v0/articles_controller.rb
+++ app/controllers/api/v0/articles_controller.rb
@@ -4,6 +4,7 @@ class Api::V0::ArticlesController < ApplicationController
   def index
     articles = Article.all
     render json: articles, each_serializer: Articles::IndexSerializer
+    
   end
 
   def create
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, :body, presence: true
   belongs_to :author, class_name: 'User'
+  scope :city, -> (city_id) { where city_id: city_id }
 end
